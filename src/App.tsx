eac097dc41e8d19e9a7fd6beb22357ebc2ab877f
leoshatrushin <leoshatrushin@gmail.com>
2023-12-01 15:45:17 +0800

    remove statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { AppProvider } from './App.provider';
 import './services/bluetooth';
+import { StatusBar } from 'react-native';
 import { Home } from './screens/Home';
 
 export default function App() {
     return (
         <AppProvider>
+            <StatusBar hidden={true} />
             <Home />
         </AppProvider>
     );
